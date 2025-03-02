4f0af969c12412ac54c050d0bb810434e4f591e2
jorge76r@gmail.com <jorge76r@gmail.com>
2025-03-02 13:08:46 -0600

    Reapply "Se cambio a expo-router"
    This reverts commit f201c0942538870adc44ee8ef27e02fe5aa30e01.

--- agendafamiliar/app/(tabs)/home.tsx
+++ agendafamiliar/app/(tabs)/home.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, Button, Image } from 'react-native';
-import TaskCard from '../../components/TaskCard';
+import TaskCard from '../../components/TaskCard';  // Ajusta la ruta si es necesario
 
-// Definimos la interfaz para el usuario
 interface User {
   email: string;
   password: string;
@@ -40,26 +39,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    paddingBottom: 60, // Ajustar el padding inferior para dejar espacio para la barra de navegación
+    paddingBottom: 60,
     backgroundColor: '#F5F7FA',
   },
   perfil: {
-    width: 150, // Ajusta el ancho del rectángulo
-    height: 150, // Ajusta la altura del rectángulo
-    marginBottom: 20,//Margen inferior para no estar pegados
-    borderRadius: 10, // Redondea los bordes de la imagen
-    borderWidth: 2, // Añade un borde a la imagen
-    borderColor: 'gray', // Color del borde
-    alignSelf: 'center', // Centrar la imagen horizontalmente
-},
-text: {
-  fontSize: 18, // Tamaño de fuente más grande para mayor énfasis
-  fontWeight: 'bold', // Texto en negrita para resaltar
-  color: '#333333', // Color de texto más oscuro para mejor legibilidad
-  textAlign: 'center', // Centrar el texto horizontalmente
-  marginBottom: 20, // Mantén el espacio inferior
-},
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'gray',
+    alignSelf: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333333',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
   list: {
-    paddingBottom: 60, // Ajustar el padding inferior a la lista también
+    paddingBottom: 60,
   },
 });
